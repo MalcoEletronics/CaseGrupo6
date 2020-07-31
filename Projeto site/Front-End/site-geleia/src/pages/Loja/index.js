a9f7e377464874f31760d8c5fffc37f3df8926cb
Malco Freitas <malco.eletronica@poli.ufrj>
2020-07-30 23:57:33 -0300

    Procurando o motivo do componente sellProduct não estar sendo importado

--- Projeto site/Front-End/site-geleia/src/pages/Loja/index.js
+++ Projeto site/Front-End/site-geleia/src/pages/Loja/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import sellProduct from "./../../components/sellProduct/index.js"
+import sellProduct from './../../components/sellProduct/index.js'
  
 import './styles.css';
 export default class Loja extends Component {
