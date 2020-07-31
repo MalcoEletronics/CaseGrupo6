77be76566c825c3ac99aa929034c56eaefc4851c
Malco Freitas <malco.eletronica@poli.ufrj>
2020-07-30 23:35:45 -0300

    Inserindo componentização de produto

--- Projeto site/Front-End/site-geleia/src/pages/Loja/index.js
+++ Projeto site/Front-End/site-geleia/src/pages/Loja/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import sellProduct from "./../../components/sellProduct/index.js"
+ 
 import './styles.css';
 export default class Loja extends Component {
     render(){
         return (
             <body>
                 olá estamos na loja
+                <sellProduct/>
+
             </body>
         );
     }
